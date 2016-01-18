ed986ae5f9a23790f2eb453dac40633590a3dabb
John Murray <me@johnmurray.io>
2016-01-18 18:44:27 -0500

    style update

--- gateway.go
+++ gateway.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
-
 /**
  * File: gateway.go
  *
  * The gateway is the main processing center for NBA. This is where the decisions
  * are made on which alerts are proxied, cached, discarded, set OK, etc.
  */
+
+import (
+	"fmt"
+)
 
 type Gateway struct {
 	registry *Registry
